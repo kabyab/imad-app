7a7797cd3ad45c3074cb1cb8632179ec3a723623
Kabya Banerjee <bandokabs@gmail.com>
2017-08-20 11:13:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,18 @@ content.innerHTML = 'Javascript changed me!!';
 
 //Move image
 var img = document.getElementById('madi');
+var marginLeft = 0;
 
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function() {
+  var interval = setInterval(moveRight, 100);  
+};
+
+/*
 img.onclick = function () {
   img.style.marginLeft = '100px';  
-};
+};*/
