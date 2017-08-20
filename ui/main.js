00c8cea1d49da0396694a0523810aab0c7077ccc
Kabya Banerjee <bandokabs@gmail.com>
2017-08-20 14:12:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,23 @@ var submit = document.getElementById('submit');
 var list = document.getElementById('namelist');
 
 submit.onclick = function() {
-    var entry = document.createElement('li');
-    //entry.innerHTML = nametext;
-    entry.appendChild(document.createTextNode(nametext));
-    list.appendChild(entry);
+    
+    var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var entry = '';
+            for(var i = 0; i < names.length; i++) {
+                entry = '<li>' + names[i] + '</li>';
+            }
+            list.innerHTML = entry;
+        }
+    }
+  }; 
+    request.open('GET', 'http://bandokabs.imad.hasura-app.io/submit-name/' + nametext, true);
+    request.send(null);
 };
