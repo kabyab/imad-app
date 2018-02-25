b3b253fcb13e7e55eae9f5400a18f80c0d9b338a
kabyab <bandokabs@gmail.com>
2018-02-25 16:50:22 +0530

    Corrected Submit list error

--- ui/main.js
+++ ui/main.js
@@ -17,17 +17,41 @@ button.onclick = function() {
     }  
   };
   
-  request.open('GET', 'http://bandokabs.imad.hasura-app.io/counter', true);
+  if('withCredentials' in request) {
+    request.withCredentials = true;
+    request.open('GET', 'http://localhost:8080/counter', true);
+  } else if (typeof XDomainRequest != "undefined") {
+    request.open('GET', 'http://localhost:8080/counter');
+  } else {
+    request = null;
+    console.log("CORS not supported!!!");
+  }
   request.send(null);
 };
 
-var nametext = document.getElementById('name').value;
 var submit = document.getElementById('submit');
 var list = document.getElementById('namelist');
 
 submit.onclick = function() {
-    var entry = document.createElement('li');
-    //entry.innerHTML = nametext;
-    entry.appendChild(document.createTextNode(nametext));
-    list.appendChild(entry);
+    
+    var request = new XMLHttpRequest();
+  
+  // Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var entry = '';
+            for(var i = 0; i < names.length; i++) {
+                entry = '<li>' + names[i] + '</li>';
+            }
+            list.innerHTML = entry;
+        }
+    }
+  }; 
+    var nametext = document.getElementById('name').value;
+    var submitUrl = 'http://localhost:8080/submit-name/' + nametext;
+    request.open('GET', submitUrl, true);
+    request.send(null);
 };
